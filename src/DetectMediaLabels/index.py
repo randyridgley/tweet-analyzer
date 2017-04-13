0a64ac7d67f36f922dd9923c77437f0742757c62
Randy Ridgley <rridgley@amazon.com>
2017-04-12 18:08:36 -0700

    fix son parsing

--- src/DetectMediaLabels/index.py
+++ src/DetectMediaLabels/index.py
@@ -26,14 +26,15 @@ def detect_labels(image, max_labels=10, min_confidence=95):
     return response['Labels']
 
 def handler(event, context):
-    for media in event['entities']['media']:
-        image_data = BytesIO(urlopen(media['media_url_https']).read())
+    tweet = json.loads(event)
+    for media in tweet['entities']['media']:
+        image_data = BytesIO(urlopen(tweet['media_url_https']).read())
 
         labels = {}
         for label in detect_labels(image_data.getvalue(), labels, confidence):
-            label['media_url_https'] = media['media_url_https']
+            label['media_url_https'] = tweet['media_url_https']
             labels.append(label)
 
         event['labels'] = labels
-    return json.dumps(event)
+    return json.dumps(tweet)
 
